6fcb81a6a951ced61c5abfff6452e9f40c08f27b
Modupe Akanni <modupe775@gmail.com>
2025-01-12 13:10:12 +0100

    feat: enhance banking system with error handling for deposits and withdrawals

--- banking-system/main.rs
+++ banking-system/main.rs
@@ -13,16 +13,36 @@ fn main () {
         balance: 2000.0,
     };
 
-    account.deposit(500.0);
-    account2.withdraw(1000.0);
+    match account.deposit(500.0) {
+        Ok(()) => println!("Deposit successful!"),
+        Err(err) => println!("Failed to deposit: {}", err),
+    };
+
+    match account2.withdraw(1000.0) {
+        Ok(()) => println!("Withdrawal successful!"),
+        Err(err) => println!("Failed to withdraw: {}", err),
+    };
+
+    println!("Account balance: {}", account.balance());
+    println!("Account 2 balance: {}", account2.balance());
+
+    match account.deposit(-500.0) {
+        Ok(()) => println!("Deposit successful!"),
+        Err(err) => println!("Failed to deposit: {}", err),
+    }
+
+    match account2.withdraw(2500.0) {
+        Ok(()) => println!("Withdrawal successful!"),
+        Err(err) => println!("Failed to withdraw: {}", err),
+    }
 
     println!("Account balance: {}", account.balance());
     println!("Account 2 balance: {}", account2.balance());
 }
 
 trait Account {
-    fn deposit(&mut self, amount: f64);
-    fn withdraw(&mut self, amount: f64);
+    fn deposit(&mut self, amount: f64)  -> Result<(), String>;
+    fn withdraw(&mut self, amount: f64) -> Result<(), String>;
     fn balance(&self) -> f64;
 }
 
@@ -33,12 +53,20 @@ struct BankAccount {
 }
 
 impl Account for BankAccount {
-    fn deposit(&mut self, amount: f64) {
+    fn deposit(&mut self, amount: f64) -> Result<(), String> {
+        if amount < 0.0 {
+            return Err("Can't deposit negative amount".to_string());
+        }
         self.balance += amount;
+        Ok(())
     }
 
-    fn withdraw(&mut self, amount: f64) {
+    fn withdraw(&mut self, amount: f64) -> Result<(), String> {
+        if amount > self.balance {
+            return Err("Insufficient funds".to_string());
+        }
         self.balance -= amount;
+        Ok(())
     }
 
     fn balance(&self) -> f64 {
